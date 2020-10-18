6e54c7ab85db45419212f145a8c83b3bd7856a91
SantiagoMDC <santiagod000@gmail.com>
2020-10-17 19:44:57 -0500

    primer commit

---
+++ Entity/Persona.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Entity
+{
+    public class Persona
+    {
+        public string identificacion  {get;set; }
+        public string nombre { get;set; }
+        public string apellidos { get;set; }
+        public string sexo { get;set; }
+        public int edad { get;set; }
+        public string departamento { get;set; }
+        public string ciudad { get;set; }
+        public int valorDeApoyo { get;set; }
+        public string modalidadDeApoyo { get;set; }
+        public string fecha { get;set; }
+    
+    }
+}
